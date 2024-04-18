b33170700aa11fb62b3a862664be0d7cb1066987
Marco Carraro <carra.rules@gmail.com>
2024-04-18 15:17:12 +0200

    Initial Commit

---
+++ AI Prompt Generator/AI_Prompt_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Prompt_GeneratorApp.swift
+//  AI Prompt Generator
+//
+//  Created by Manhattan on 18/04/24.
+//
+
+import SwiftUI
+
+@main
+struct AI_Prompt_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI Prompt Generator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AI Prompt Generator
+//
+//  Created by Manhattan on 18/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
