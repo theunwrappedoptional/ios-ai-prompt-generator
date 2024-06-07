342f6d26deecf0640d738198543c79695ad3f12e
Manhattan <carra.rules@gmail.com>
2024-06-07 12:24:40 +0200

    Add UI labels and refactoring

---
+++ AI Prompt Generator/AttributesFormSection.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  AI Prompt Generator
+//
+//  Created by Manhattan on 07/06/24.
+//
+
+import SwiftUI
+
+struct AttributesFormSection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AttributesFormSection()
+}
